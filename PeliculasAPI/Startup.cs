cbdf5ca690f6360fb1dd4ef3169053138428c9ca
EngelberAmaya <engelberamaya@gmail.com>
2022-02-10 14:04:54 -0400

    Creating the Movie Genre Controller

--- PeliculasAPI/Startup.cs
+++ PeliculasAPI/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace PeliculasAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
