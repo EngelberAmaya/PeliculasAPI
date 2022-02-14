f9c0e1c1c7a1daa415b620e7a7f80b78b97fd705
EngelberAmaya <engelberamaya@gmail.com>
2022-02-14 11:22:03 -0400

    Entity Movie (CRUD)

---
+++ PeliculasAPI/DTOs/PeliculaCreacionDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using PeliculasAPI.Validaciones;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.DTOs
+{
+    public class PeliculaCreacionDTO : PeliculaPatchDTO
+    {
+        [PesoArchivoValidacion(PesoMaximoEnMegaBytes: 4)]
+        [TipoArchivoValidacion(GrupoTipoArchivo.Imagen)]
+        public IFormFile Poster { get; set; }
+    }
+}
